48ec29c615f7b207d474640cff42adf319efe57d
Pablo Suarez <pablo.suarez@imagineapps.co>
2021-04-11 00:19:32 -0500

    bug fix

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AccountInfo } from '@azure/msal-common';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private msalService: MsalService) { }
+
+  name: string;
+  username: string;
+  account: AccountInfo;
 
   ngOnInit(): void {
+    this.account = this.msalService.instance.getActiveAccount();
+    if (this.account != null) {
+      this.name = this.account.name;
+      this.username = this.account.username;
+    }
   }
 
 }
