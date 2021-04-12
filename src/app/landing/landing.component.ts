d071055d18a6671bd031baa508811f09da5f7656
Pablo Suarez <pablo.suarez@imagineapps.co>
2021-04-12 13:57:13 -0500

    login redirect

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -11,22 +11,33 @@ export class LandingComponent implements OnInit {
 
   constructor(private msalService: MsalService) { }
 
+  ngOnInit(): void {
+    this.msalService.instance.handleRedirectPromise().then(
+      res => {
+        if (res != null && res.account != null) {
+          this.msalService.instance.setActiveAccount(res.account);
+        }
+      }
+    );    
+  }
+
   isLoggedIn(): boolean {
     return this.msalService.instance.getActiveAccount() != null;
   }
 
   login() {
+    /*
+    //PopUp
     this.msalService.loginPopup().subscribe( (response: AuthenticationResult) => {
       this.msalService.instance.setActiveAccount(response.account);
     });
+    */
+    //Redirect
+    this.msalService.loginRedirect();
   }
 
   logout() {
     this.msalService.logout();
-  }
-
-  ngOnInit(): void {
-    
   }
 
 }
